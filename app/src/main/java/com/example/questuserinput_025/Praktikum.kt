98ee8f02042afcc77ed4e1478c0883504187ce7c
humannocturnal <syaputrar374@gmail.com>
2025-11-01 18:26:05 +0700

    menambahkan teks judul pada Box sebagai header

--- app/src/main/java/com/example/questuserinput_025/Praktikum.kt
+++ app/src/main/java/com/example/questuserinput_025/Praktikum.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -12,8 +14,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun Praktikum (modifier: Modifier
@@ -38,7 +43,12 @@ fun Praktikum (modifier: Modifier
             .background(colorResource(id = R.color.purlple)),
             contentAlignment = Alignment.BottomStart
         ){
-
+            Text("formulir pendaftaran",
+                modifier = Modifier.padding(15.dp),
+                color = Color.White,
+                fontSize = 25.sp,
+                fontWeight = FontWeight.SemiBold
+            )
         }
     }
 }
