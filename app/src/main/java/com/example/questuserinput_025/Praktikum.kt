1bc4d6c916d0b6d63f72f64a54199564bff58428
humannocturnal <syaputrar374@gmail.com>
2025-11-01 18:31:38 +0700

    membuat container utama form dengan latar putih

--- app/src/main/java/com/example/questuserinput_025/Praktikum.kt
+++ app/src/main/java/com/example/questuserinput_025/Praktikum.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -14,6 +15,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
@@ -49,6 +52,17 @@ fun Praktikum (modifier: Modifier
                 fontSize = 25.sp,
                 fontWeight = FontWeight.SemiBold
             )
+        }
+        Column (modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp)
+            .padding(horizontal = 2.dp, vertical = 5.dp)
+            .shadow(8.dp, shape = RoundedCornerShape(16.dp))
+            .clip(RoundedCornerShape(16.dp))
+            .height(600.dp)
+            .background(Color.White))
+        {
+
         }
     }
 }
