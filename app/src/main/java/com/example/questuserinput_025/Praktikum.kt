7b6ab3cf411bdd42d0b494d85e9da9c9f75f74de
humannocturnal <syaputrar374@gmail.com>
2025-11-01 18:11:50 +0700

    menambahkan list gender dan status serta memperbaiki styling layout Column

--- app/src/main/java/com/example/questuserinput_025/Praktikum.kt
+++ app/src/main/java/com/example/questuserinput_025/Praktikum.kt
@@ -1,12 +1,14 @@
 package com.example.questuserinput_025
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import java.lang.reflect.Modifier
-
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 
 @Composable
 fun Praktikum (modifier: Modifier
@@ -20,4 +22,12 @@ fun Praktikum (modifier: Modifier
     var alamat by remember { mutableStateOf("") }
     var jenis by remember { mutableStateOf("") }
     var status by remember { mutableStateOf("") }
+
+    val gender: List<String> = listOf("Laki-laki","Perempuan")
+    val statusoption: List<String> = listOf("Janda","Lajang","Duda")
+
+    Column (modifier = Modifier.background(colorResource(id = R.color.gray))
+    ){
+
+    }
 }
