6f87bf7a2f6191a46be636ce1433777c01e10f2f
humannocturnal <syaputrar374@gmail.com>
2025-11-01 18:02:09 +0700

    menambahkan fungsi composable Praktikum untuk inisialisasi form

---
+++ app/src/main/java/com/example/questuserinput_025/Praktikum.kt
@@ -0,0 +1,18 @@
+package com.example.questuserinput_025
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import java.lang.reflect.Modifier
+
+
+@Composable
+fun Praktikum (modifier: Modifier
+){
+    var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
+    var textJK by remember { mutableStateOf("") }
+    var textstatus by remember { mutableStateOf("") }
+}
